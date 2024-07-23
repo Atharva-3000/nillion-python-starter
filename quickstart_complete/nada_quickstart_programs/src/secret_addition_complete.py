cf2f5517b6d05846d2b37bc9654a9a753c5d57a5
Atharva-3000 <deshmukhsambhajirao9@gmail.com>
2024-07-23 05:46:13 +0000

    my new nada program

--- quickstart_complete/nada_quickstart_programs/src/secret_addition_complete.py
+++ quickstart_complete/nada_quickstart_programs/src/secret_addition_complete.py
@@ -1,12 +1,25 @@
 from nada_dsl import *
-def nada_main():
 
+def nada_main():
+    # Define the parties involved
     party1 = Party(name="Party1")
 
-    my_int1 = SecretInteger(Input(name="my_int1", party=party1))
+    # Define the secret inputs for the parties
+    secret_int1 = SecretInteger(Input(name="secret_int1", party=party1))
+    secret_int2 = SecretInteger(Input(name="secret_int2", party=party1))
+    secret_int3 = SecretInteger(Input(name="secret_int3", party=party1))
 
-    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
+    # Compute the sum of the three secret integers
+    total_sum = secret_int1 + secret_int2 + secret_int3
 
-    new_int = my_int1 + my_int2
+    # Compute the average of the three secret integers
+    average = total_sum // 3
 
-    return [Output(new_int, "my_output", party1)]
+    # Define a constant factor to multiply with the average
+    constant_factor = SecretInteger(Value(5))
+
+    # Multiply the average by the constant factor
+    final_result = average * constant_factor
+
+    # Return the final result as output
+    return [Output(final_result, "final_result_output", party1)]
